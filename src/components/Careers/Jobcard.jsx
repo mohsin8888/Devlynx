f25b6641459d4ca6b258ba9003e14dfdb1f83522
aamerhussain134 <106589207+aamerhussain134@users.noreply.github.com>
2024-09-11 11:46:03 +0500

    button update

--- src/components/Careers/Jobcard.jsx
+++ src/components/Careers/Jobcard.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-
+import React from "react";
+import Button from "../Buttons/Button";
+import { Link } from "react-router-dom";
 export const Jobcard = ({
-    type,
-    value,
-     onClick ,
-     CustomClass,
-     Cardheading,
-     Cardpara,
-
+  type,
+  value,
+  onClick,
+  CustomClass,
+  Cardheading,
+  Cardpara,
 }) => {
   return (
     <div class={`flex justify-center items-center pb-8 ${CustomClass}`}>
-        <div class="bg-white shadow-xl rounded-lg p-6 w-[80%]">
-            <h3 class="text-xl font-semibold mb-4">{Cardheading}</h3>
-            <p class="text-gray-600 mb-4">
-               {Cardpara}
-            </p>
-            <button type={type} value={value} className="bg-blue-600 text-white px-6 py-2  rounded hover:bg-blue-700">Apply Now</button>
-        </div>
-
-</div>
-  )
-}
+      <div class="bg-white shadow-xl rounded-lg p-6 w-[80%]">
+        <h3 class="text-xl font-semibold mb-4">{Cardheading}</h3>
+        <p class="text-gray-600 mb-4">{Cardpara}</p>
+        <Link to="mailto:connect@devlynxtech.com">
+          <Button
+            value={"Apply Now"}
+            customClass={
+              "rounded-md border border-transparent  w-40 hover:bg-white hover:text-[#2B76C1] hover:font-[400] hover:border-[#2B76C1] transition-all duration-300"
+            }
+          />
+        </Link>
+      </div>
+    </div>
+  );
+};
